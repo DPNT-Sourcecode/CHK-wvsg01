b8583479b170c50eb164e8595288904dbb9bcf67
sbx_user1051 <sbx_user1051@169.254.9.253>
2020-12-12 16:47:40 +0000

    Sat Dec 12 16:47:40 UTC 2020

--- test/solutions/CHK/test.js
+++ test/solutions/CHK/test.js
@@ -4,12 +4,19 @@ var it = mocha.it
 var assert = require('assert');
 const checkout = require('../../../lib/solutions/CHK/checkout');
 
-const request1 = 'ABCD'
-const request2 = 'AAABBCD'
+const invalidRequest1 = null
+const invalidRequest2 = 'INVALID'
+
+const validRequest1 = 'ABCD'
+const validRequest2 = 'AAABBCD'
 
 describe('Checkout', function () {
   it('Should return -1 if input is invalid', function () {
-    assert.equal(checkout(null), -1);
+    assert.equal(checkout(invalidRequest1), -1);
+  });
+
+  it('Should return -1 if input is invalid', function () {
+    assert.equal(checkout(invalidRequest2), -1);
   });
 
   // it('Should return 0 if input is empty', function () {
@@ -20,3 +27,4 @@ describe('Checkout', function () {
   //   assert.equal(checkout(request1), 115);
   // });
 });
+
