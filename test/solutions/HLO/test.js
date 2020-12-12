63d42abde654cb9e44e596ee335af87fdc5de1ef
sbx_user1051 <sbx_user1051@169.254.9.253>
2020-12-12 16:27:02 +0000

    Sat Dec 12 16:27:02 UTC 2020

---
+++ test/solutions/HLO/test.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
+var assert = require('assert');
+const hello = require('../../../lib/solutions/HLO/hello');
+
+describe('Hello challenge: return param', function () {
+  it('Should return', function () {
+    assert.equal(hello('Hello'), 'Hello');
+  });
+});
