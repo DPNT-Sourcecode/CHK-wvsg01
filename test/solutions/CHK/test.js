628aca90d927011f33769f3b5f0806bebff0f25b
sbx_user1051 <sbx_user1051@169.254.9.253>
2020-12-12 16:46:14 +0000

    Sat Dec 12 16:46:14 UTC 2020

--- test/solutions/CHK/test.js
+++ test/solutions/CHK/test.js
@@ -9,8 +9,12 @@ const request2 = 'AAABBCD'
 
 describe('Checkout', function () {
   it('Should return -1 if input is invalid', function () {
-    assert.equal(checkout(''), -1);
+    assert.equal(checkout(null), -1);
   });
+
+  // it('Should return 0 if input is empty', function () {
+  //   assert.equal(checkout(''), 0);
+  // });
 
   // it('Should return the price without promotions', function () {
   //   assert.equal(checkout(request1), 115);
