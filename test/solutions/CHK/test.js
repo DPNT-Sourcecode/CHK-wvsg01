3b39a2d929dee01a6260d2820e5ee025fed25712
sbx_user1051 <sbx_user1051@169.254.117.53>
2020-12-12 18:49:06 +0000

    Sat Dec 12 18:49:06 UTC 2020

--- test/solutions/CHK/test.js
+++ test/solutions/CHK/test.js
@@ -15,27 +15,50 @@ describe('Checkout: invalid requests', function () {
 })
 
 describe('Checkout: valid requests', function () {
-  it('Should return 200', function () {
-    assert.equal(checkout('AAAAA'), 200)
+  // it('Should return 200', function () {
+  //   assert.equal(checkout('AAAAA'), 200)
+  // })
+
+  // it('Should return 330', function () {
+  //   assert.equal(checkout('AAAAAAAA'), 330)
+  // })
+
+  // it('Should return 245', function () {
+  //   assert.equal(checkout('AAAAABB'), 245)
+  // })
+
+  // it('Should return 320', function () {
+  //   assert.equal(checkout('AAAAABBCDE'), 320)
+  // })
+
+  // it('Should return 95', function () {
+  //   assert.equal(checkout('BBEE'), 95)
+  // })
+
+  // it('Should return 80', function () {
+  //   assert.equal(checkout('EE'), 80)
+  // })
+
+  it('Should return 160', function () {
+    assert.equal(checkout('EEEEBB'), 160)
   })
 
-  it('Should return 330', function () {
-    assert.equal(checkout('AAAAAAAA'), 330)
-  })
-
-  it('Should return 245', function () {
-    assert.equal(checkout('AAAAABB'), 245)
-  })
-
-  it('Should return 320', function () {
-    assert.equal(checkout('AAAAABBCDE'), 320)
-  })
-
-  it('Should return 95', function () {
-    assert.equal(checkout('BBEE'), 95)
-  })
-
-  it('Should return 80', function () {
-    assert.equal(checkout('EE'), 80)
-  })
+  // it('Should return 280', function () {
+  //   assert.equal(checkout('ABCDEABCDE'), 280)
+  // })
 })
+
+
+/*
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
+*/
+
+// 40 + 40 + 40 + 40 + 30 + 30 - 60
